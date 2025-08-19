f603760765db65de3b4f9c949c1edeeaddc20769
Josefine Klasson <jossanwowar93@gmail.com>
2025-08-19 14:48:28 +0200

    111

--- övningar/övningar.js
+++ övningar/övningar.js
@@ -29,6 +29,10 @@ const indata = [
   { name: "Morot", category: "grönsak", price: 8 },
   { name: "Tomat", category: "grönsak", price: 20 }
 ]
-
-const frukt = Object.fromEntries(indata); // filtera har eller innan?
+const gruppat = indata.reduce((acc, produkt) => {
+  // kolla om acc redan har en nyckel för produkt.category
+  // annars skapa en tom array
+  // lägg sedan till produkten i den arraye
+    return acc;
+}, {});
 console.log(frukt);
